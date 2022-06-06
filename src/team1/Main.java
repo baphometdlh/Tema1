672132d83b54502c41f56da4b24c0e199c7afcae
baphomet_dlh <baphomet_dlh@me.com>
2022-06-06 22:48:24 +0200

    tema1 por daniel lopez

---
+++ src/team1/Main.java
@@ -0,0 +1,18 @@
+package team1;
+
+public class Main {
+    public static void main(String[] args) {
+        byte x = 0;
+        short y = 1;
+        int z = 2;
+        long l = 3;
+        float f = 0.01f;
+        double d = 1.2;
+        boolean a = true;
+        char c = 'a';
+        String palabra = "hola";
+
+        System.out.println(x  + ", " + y + ", " + z + ", " + l + ", " + f );
+        System.out.println(d + ", " + a + ", " + c + ", " + palabra);
+    }
+}
